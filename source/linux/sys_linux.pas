b590fa6c91f7259035777d6e9823888818ff1d47
Popa Marius Adrian <mapopa@gmail.com>
2012-02-01 08:41:08 +0200

    cleanup

--- source/linux/sys_linux.pas
+++ source/linux/sys_linux.pas
@@ -28,11 +28,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 interface
 
-<<<<<<< HEAD
-uses q_shared_add , sysutils , Common;
-=======
+
 uses q_shared_add , SysUtils , Common ,unix;
->>>>>>> 0c4caab0417c2ec96ae7e8ee20fc2a435b560750
+
 
 {.$include <unistd.h>}
 {.$include <signal.h>}
